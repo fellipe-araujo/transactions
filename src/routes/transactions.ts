c2a764482c3c77a79bfea16fa106c28f97e099a9
Fellipe Araujo <fellipe.eng.soft@gmail.com>
2026-06-25 15:19:50 -0300

    feat: add list transactions routes

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -4,6 +4,26 @@ import { randomUUID } from 'node:crypto'
 import { db } from '../database'
 
 export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await db('transactions').select()
+
+    return { transactions }
+  })
+
+  app.get('/:id', async (request) => {
+    const getTransactionsParamsSchema = z.object({
+      id: z.uuid(),
+    })
+
+    const { id } = getTransactionsParamsSchema.parse(request.params)
+
+    const transaction = await db('transactions').where('id', id).first()
+
+    return {
+      transaction,
+    }
+  })
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
